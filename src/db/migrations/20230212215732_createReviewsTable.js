b9bb952a425ea53edd178e9a593c83424edb4403
Jesse Vo <110706695+jdotvo@users.noreply.github.com>
2023-02-17 22:45:17 -0800

    Update migration file

--- src/db/migrations/20230212215732_createReviewsTable.js
+++ src/db/migrations/20230212215732_createReviewsTable.js
@@ -3,22 +3,18 @@ exports.up = function (knex) {
     table.increments("review_id").primary();
     table.text("content");
     table.integer("score");
-    table.string("rating");
-
     table.integer("movie_id").unsigned().notNullable();
     table
       .foreign("movie_id")
       .references("movie_id")
       .inTable("movies")
       .onDelete("cascade");
-  
     table.integer("critic_id").unsigned().notNullable();
     table
       .foreign("critic_id")
       .references("critic_id")
       .inTable("critics")
       .onDelete("cascade");
-   
     table.timestamps(true, true);
   });
 };
