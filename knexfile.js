eb3e5f88438a68fb5066b80bef756b1946cf1786
Jesse Vo <110706695+jdotvo@users.noreply.github.com>
2023-02-19 19:44:52 -0800

    Added postgres url

--- knexfile.js
+++ knexfile.js
@@ -3,7 +3,7 @@ const path = require("path");
 require("dotenv").config();
 
 const {
-  DATABASE_URL = "postgresql://postgres@localhost/postgres",
+  DATABASE_URL = "postgres://lwpkugfx:ETwlWDlbA5jd0qsJLi_mYUuKO6ZSapb_@drona.db.elephantsql.com/lwpkugfx",
 } = process.env;
 
 module.exports = {
